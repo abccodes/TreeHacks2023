b71bffa05e5f15320c93ebac614e3d760b112efa
Aidan <abayercalvert@gmail.com>
2023-02-19 10:17:05 -0800

    webpage

--- HTMLYou/firebase/functions/index.js
+++ HTMLYou/firebase/functions/index.js
@@ -1,6 +1,6 @@
-import fetch from 'node-fetch';
-import functions from "firebase-functions";
-import admin from "firebase-admin";
+import fetch from 'node-fetch'
+import functions from 'firebase-functions'
+import admin from 'firebase-admin'
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
@@ -10,34 +10,34 @@ import admin from "firebase-admin";
 //   response.send("Hello from Firebase!");
 // });
 
-admin.initializeApp();
+admin.initializeApp()
 
 export const queryModel = functions.https.onRequest(async (req, res) => {
-    const prompt = req.query.prompt;
-    if (!prompt) {
-        res.status(400).send('Missing prompt');
-        return;
-    }
-    console.log(prompt);
+  const prompt = req.query.prompt
+  if (!prompt) {
+    res.status(400).send('Missing prompt')
+    return
+  }
+  console.log(prompt)
 
-    fetch('https://api.openai.com/v1/completions', {
-        method: 'POST',
-        headers: {
-            Authorization: `Bearer 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`,
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            "model": "davinci:ft-personal-2023-02-19-08-15-34",
-            "prompt": `${prompt} |||`,
-            "max_tokens": 1262,
-            "temperature": 0.4,
-            "stop": "###"
-        })
-    })
+  fetch('https://api.openai.com/v1/completions', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer 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`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      model: 'davinci:ft-personal-2023-02-19-08-15-34',
+      prompt: `${prompt} |||`,
+      max_tokens: 1262,
+      temperature: 0.4,
+      stop: '###',
+    }),
+  })
     .then(async (response) => {
-        res.status(200).send(await response.text());
+      res.status(200).send(await response.text())
     })
     .catch((error) => {
-        console.error(error);
+      console.error(error)
     })
-});
+})
--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,56 +1,280 @@
 import logo from './logo.svg'
 import './App.css'
 import styled from 'styled-components'
-import { motion } from 'framer-motion'
+import React from 'react'
+// import fetch from 'node-fetch'
 
-const PageContainer = styled.div`
+const FormSection = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  flex-wrap: wrap;
   justify-content: center;
-  height: 100vh;
-`
+  align-items: center;
 
-const Title = styled.h1`
-  font-size: 4rem;
-  text-align: center;
-  align-self: top;
-  margin-bottom: 3rem;
-  color: #efa9ae;
-`
+  margin: 1rem auto;
+  margin-top: 50px;
+  margin-bottom: 25px;
 
-const Description = styled.p`
-  font-size: 2rem;
-  text-align: center;
-  margin-bottom: 4rem;
-  color: #f6c1c1;
-`
-
-const Button = styled.button`
-  background-color: #ff99cc;
+  width: fit-content;
+  padding: 25px;
   color: #fff;
-  font-size: 2rem;
-  padding: 1rem 2rem;
-  border: none;
-  border-radius: 20px;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
+  background: linear-gradient(-45deg, #37829f, #ae71ae, #2f57cf, #cf63c4);
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
 
-  &:hover {
-    background-color: #f6c1c1;
+  // border: 2px solid #2d2d2d;
+  border-radius: 5rem;
+  box-shadow: 0 0 5px 5px #32323240;
+  @media screen and (max-width: 600px) {
+    padding: 15px;
   }
 `
 
+const FormSectionRes = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  text-align: left;
+  align-items: flex-start;
+
+  margin: 1rem auto;
+  margin-top: 50px;
+  margin-bottom: 25px;
+
+  width: fit-content;
+  padding: 25px;
+  color: #fff;
+  background: linear-gradient(-45deg, #37829f, #ae71ae, #2f57cf, #cf63c4);
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+
+  // border: 2px solid #2d2d2d;
+  border-radius: 5rem;
+  box-shadow: 0 0 5px 5px #32323240;
+  @media screen and (max-width: 600px) {
+    padding: 15px;
+  }
+`
+
+const PageContainer = styled.div`
+  display: flex;
+  // flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  align-text: center;
+`
+
+const Title = styled.h1`
+  flex: 1;
+  align-self: center;
+  text-align: center;
+  // margin: 1rem auto;
+  margin-top: 7.5rem;
+  font-size: 4rem;
+  color: #000;
+  font-family: Roboto;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+`
+
+const GenTitle = styled.h1`
+  flex: 1;
+  align-self: center;
+  text-align: center;
+  // margin: 1rem auto;
+  // margin-top: 7.5rem;
+  font-size: 4rem;
+  color: #000;
+  font-family: Roboto;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+`
+
+const Description = styled.p`
+  font-size: 2.5rem;
+  text-align: center;
+  margin-bottom: 5rem;
+  color: #000;
+  font-family: Roboto;
+`
+
+const Footer = styled.div`
+  flex: 1;
+  font-size: 2.5rem;
+  bottom: 0;
+`
+
+const Section = styled.div`
+  justify-content: center;
+  align-items: center;
+  align-text: center;
+  height: auto;
+  width: 100%;
+  position: relative;
+  // overflow: hidden;
+  // background-color: #121113;
+  @media screen and (max-width: 950px) {
+    height: 100vh;
+  }
+  @media screen and (max-width: 540px) {
+    height: 120vh;
+  }
+  @media screen and (max-width: 450px) {
+    height: auto;
+  }
+`
+const Box = styled.div`
+  justify-content: center;
+  align-items: center;
+  align-text: center;
+  height: auto;
+  width: 100%;
+  position: relative;
+  // overflow: hidden;
+  background-color: #121113;
+  @media screen and (max-width: 950px) {
+    height: 100vh;
+  }
+  @media screen and (max-width: 540px) {
+    height: 120vh;
+  }
+  @media screen and (max-width: 450px) {
+    height: auto;
+  }
+`
+
+const GeneratedText = styled.p`
+  flex: 1;
+  align-self: flex-start;
+  text-align: left;
+  // margin: 1rem auto;
+  // margin-top: 7.5rem;
+  font-size: 1rem;
+  color: #fff;
+  font-family: Roboto;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+  height: 250px;
+  overflow-y: scroll;
+  margin-left: 30px;
+`
+
+// <div>
+//   <PageContainer>
+//     <Title>Treehacks 2023</Title>
+//     <Description>
+//       Generate a basic personalized website in seconds with the power of AI.
+//     </Description>
+//      </PageContainer>
+
 function App() {
+  const [prompt, setPrompt] = React.useState('')
+  const [response, setResponse] = React.useState('')
+  const [generated, setGenerated] = React.useState('')
+  // 0 = not started, 1 = loading, 2 = done
+  const [loadingState, setLoadingState] = React.useState(0)
+
+  function copy() {
+    navigator.clipboard.writeText(generated)
+  }
+
+  function Generated() {
+    switch (loadingState) {
+      case 0:
+        return
+      case 1:
+        return (
+          <Title>
+            <p classname="form-header">Loading</p>
+          </Title>
+        )
+      case 2:
+        return (
+          <div>
+            <GenTitle>Response</GenTitle>
+            <div classname=" center">
+              <button onClick={copy} className="btn">
+                Copy
+              </button>
+            </div>
+            <hr />
+            <GeneratedText classname="form-header">
+              <pre>
+                <code>{generated}</code>
+              </pre>
+            </GeneratedText>
+            <hr />
+          </div>
+        )
+      default:
+        console.error('invalid loading state detected')
+        return
+    }
+  }
+
+  async function generateHTML(e) {
+    e.preventDefault()
+    if (prompt == '') {
+      alert('You need to insert a prompt!')
+      return
+    }
+    console.log('asdfasdf')
+    setLoadingState(1)
+    const response = await fetch(
+      'https://acoustic-lapwing-256.convex.site/promptModel',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'text/plain',
+        },
+        body: JSON.stringify({
+          prompt: prompt,
+        }),
+      },
+    )
+    const responseJSON = await response.json()
+    const generatedHtml = responseJSON.html
+    setGenerated(generatedHtml)
+    setLoadingState(2)
+  }
+
+  function handleChange(e) {
+    e.preventDefault()
+    setPrompt(e.target.value)
+  }
   return (
-    <PageContainer className="bg">
-      <Title>Welcome to Our Website</Title>
-      <Description>
-        We're a friendly and bubbly team of developers creating cool stuff for
-        you.
-      </Description>
-      <Button>Get Started</Button>
-    </PageContainer>
+    <Section>
+      <Title>TreeHacks 2023</Title>
+      <form>
+        <FormSection>
+          <p className="form-header">The Leviathan</p>
+          <input
+            className="form-input h-10"
+            type="text"
+            placeholder="Generate a website in seconds. Input a prompt and a website will be
+          generated."
+            // name="name"
+            required
+            onChange={handleChange}
+          />
+          <button
+            onClick={(e) => {
+              generateHTML(e)
+            }}
+            className="btn"
+          >
+            Generate
+          </button>
+        </FormSection>
+      </form>
+
+      <FormSectionRes>
+        <Generated />
+      </FormSectionRes>
+    </Section>
   )
 }
 
